c3dd8eaed79f2cf7bd4f8b2a69d70eb1c592a546
BruckyZ <bruckzewge1gmail.com>
2017-10-11 16:00:57 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int mynumber;
+        double myothernumber;
+
+        mynumber=2;
+        myothernumber=1.7938;
+
+
+        System.out.println("mynumber value is : " + mynumber);
+        System.out.println("myothernumber value is : " + myothernumber);
+    }
+}
+
